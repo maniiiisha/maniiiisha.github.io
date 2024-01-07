c0d639398109d9cf3a085a0e83cb1aee01ec22d1
Manisha Singh <manishaajitkumarsingh@gmail.com>
2024-01-07 14:53:17 +0530

    updated files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-tilt'],
+    },
+  },
 })
